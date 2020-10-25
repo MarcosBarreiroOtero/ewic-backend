9f212afe533bca183bbcf8b4a2089f06c5099146
Marcos <marcos.barreiro.otero@udc.es>
2020-10-25 19:52:48 +0100

    Plantillas para los emails de reservas y comienzo de internacionalizacion

--- src/main/java/es/ewic/backend/AppApplication.java
+++ src/main/java/es/ewic/backend/AppApplication.java
@@ -1,17 +1,12 @@
 package es.ewic.backend;
 
 import java.util.TimeZone;
-import java.util.concurrent.Executor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication(exclude = { HibernateJpaAutoConfiguration.class })
-@EnableAsync
 public class AppApplication {
 
 	public static void main(String[] args) {
@@ -22,17 +17,6 @@ public class AppApplication {
 		System.setProperty("server.servlet.context-path", "/ewic");
 		SpringApplication.run(AppApplication.class, args);
 
-	}
-
-	@Bean
-	public Executor taskExecutor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(2);
-		executor.setMaxPoolSize(2);
-		executor.setQueueCapacity(500);
-		executor.setThreadNamePrefix("Ewic-");
-		executor.initialize();
-		return executor;
 	}
 
 }
---
+++ src/main/java/es/ewic/backend/configuration/SpringConfiguration.java
@@ -0,0 +1,42 @@
+package es.ewic.backend.configuration;
+
+import java.util.Locale;
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableAsync
+@EnableWebMvc
+public class SpringConfiguration implements WebMvcConfigurer {
+
+	// Enable assign functions
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("Ewic-");
+		executor.initialize();
+		return executor;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasenames("messages/message");
+		source.setDefaultLocale(Locale.ENGLISH);
+		source.setUseCodeAsDefaultMessage(true);
+
+		return source;
+	}
+
+}
